396e1388550c1600b219a65986bed91af7ca5bca
JackDilger <jack.dilger1995@gmail.com>
2022-07-30 14:28:35 +0000

    initial commit

---
+++ spins_and_needles/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for spins_and_needles project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spins_and_needles.settings')
+
+application = get_wsgi_application()
