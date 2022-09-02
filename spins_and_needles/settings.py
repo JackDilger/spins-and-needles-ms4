dbb007f6eadcb8c1c4d4bda1869a5457230c4d1e
JackDilger <jack.dilger1995@gmail.com>
2022-09-02 21:17:01 +0000

    Removed secret key and set debug

--- spins_and_needles/settings.py
+++ spins_and_needles/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uclrh6&3984&4*q*qr+rw5+89rhq-ymjumcxed-9w=157tj(y='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.inviron
 
 ALLOWED_HOSTS = ['spins-and-needles.herokuapp.com', 'localhost']
 
