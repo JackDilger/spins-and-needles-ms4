6b682e58795cb0390ba7ceacf328b1ce1cb4c60c
JackDilger <jack.dilger1995@gmail.com>
2022-09-02 07:24:33 +0000

    Create full blog comments functionality to blog posts and update style

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CommentForm
+from profiles.models import UserProfile
 
 
 def posts(request):
@@ -15,12 +17,32 @@ def posts(request):
 
 
 def post_detail(request, slug):
-    """ A view for each blog post """
+    """
+    A view for each blog post
+    """
 
+    template_name = 'blog/post_detail.html'
     post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
 
-    context = {
-        'post': post,
-    }
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        if request.user.is_authenticated:
+            profile = UserProfile.objects.get(user=request.user)
+            comment_form = CommentForm(initial={
+                'email': profile.user.email,
+                'name': profile.user.username
+                })
+        else:
+            comment_form = CommentForm()
 
-    return render(request, 'blog/post_detail.html', context)
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
