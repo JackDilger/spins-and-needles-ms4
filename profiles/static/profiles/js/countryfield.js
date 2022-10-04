959b1a42ea9877c0cbe0bfdb02ba2e966326c42c
JackDilger <jack.dilger1995@gmail.com>
2022-10-04 18:16:23 +0000

    Update placeholder text to fix accesibilty issue in profiles page

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,11 +1,11 @@
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
-    $('#id_default_country').css('color', '#aab7c4');
+    $('#id_default_country').css('color', '#000');
 };
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
-        $(this).css('color', '#aab7c4');
+        $(this).css('color', '#000');
     } else {
         $(this).css('color', '#000');
     }
