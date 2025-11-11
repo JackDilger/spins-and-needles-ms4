14b0731f17ff5d91fbb865c9e5082eeedccc2820
Jack Dilger <jack.dilger1995@gmail.com>
2025-11-11 17:29:32 +0000

    Improve profile page UX: remove autofocus and update order history table styling

--- profiles/forms.py
+++ profiles/forms.py
@@ -22,7 +22,6 @@ class UserProfileForm(forms.ModelForm):
             'default_county': 'County, State or Locality',
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
